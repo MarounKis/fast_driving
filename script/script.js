95a14e082fb78fb3ec8e9ba681632de380711b79
Maryna_Kis <ks.maryna@gmail.com>
2022-09-28 21:20:16 +0300

    First

---
+++ script/script.js
@@ -0,0 +1,13 @@
+window.onload = ()=>{
+    document.getElementById("send").onclick = function(e) {
+    document.getElementById("textInput1").value = "";
+    document.getElementById("textInput2").value = "";
+    document.getElementById("textInput3").value = "";
+    document.getElementById("textInput4").value = "";
+    document.getElementById("textInput5").value = "";
+    document.getElementById("textInput6").value = "";
+    
+    var sub="Your form has been sent. We'll contact you soon"
+		alert (sub);  
+  }
+  }
